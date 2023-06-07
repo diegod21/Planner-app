ce6818152a9d2e2f418dfd01090c4802bd165ae4
diegod21 <108184501+diegod21@users.noreply.github.com>
2023-06-07 09:58:20 -0300

    .

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 function wclick(){
-   let monthly = document.getElementById("monthly")
+   let monthly = document.getElementById("weekly")
    let day = document.getElementById("daily")
 
    monthly.classList.remove("w-25")
@@ -7,4 +7,20 @@ function wclick(){
 
    day.classList.remove("w-100")
    day.classList.add("w-25")
+}
+function removeInput(element) {
+   var input = element.previousSibling;
+   let rest = input.previousSibling
+
+   rest.remove()
+   input.remove();
+   element.remove();
+}
+function createInput(){
+   let userCall = window.prompt("Qual o nome da nova tarefa");
+   let divId = document.getElementById("inputList");
+
+   let newInput = `<div><input type="checkbox" name="listitem" id="id1">${userCall} <span onclick="removeInput(this)">X</span></div>`
+
+   divId.innerHTML += newInput
 }
